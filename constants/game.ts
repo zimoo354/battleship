5f8ef19f8a8ad2c574a5a2e9a8cf6ac74bbaef77
Carlos Isaias Ruiz <cruiz12@alumnos.uaq.mx>
2023-11-07 21:26:09 -0600

    feat: removing testing info

--- constants/game.ts
+++ constants/game.ts
@@ -1,4 +1,3 @@
 export const BOARD_SIZE = 10;
-// export const INITIAL_SHIPS = [5, 4, 3, 3];
-export const INITIAL_SHIPS = [1];
+export const INITIAL_SHIPS = [5, 4, 3, 3];
 export const MAX_SCORE = INITIAL_SHIPS.reduce((acc, curr) => acc + curr, 0);
